10244c43a7194c4ff7ae4af43a4d0d6eee323d7b
hiimtaylorjones <taylorjonessoftware@gmail.com>
2017-05-20 21:02:41 -0400

    changing env

--- config/database.php
+++ config/database.php
@@ -1,28 +1,40 @@
 <?php
 
-$host = getenv('DB_HOST');
-$username = getenv('DB_USERNAME');
-$password = getenv('DB_PASSWORD');
-$database = getenv('DB_DATABASE');
-
 return [
 
     'default' => env('DB_CONNECTION', 'pgsql'),
 
     'connections' => [
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => $host,
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
+    'sqlite' => [
+        'driver' => 'sqlite',
+        'database' => env('DB_DATABASE', database_path('database.sqlite')),
+        'prefix' => '',
+    ],
+    'mysql' => [
+        'driver' => 'mysql',
+        'host' => env('DB_HOST', 'localhost'),
+        'port' => env('DB_PORT', '3306'),
+        'database' => env('DB_DATABASE', 'forge'),
+        'username' => env('DB_USERNAME', 'forge'),
+        'password' => env('DB_PASSWORD', ''),
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix' => '',
+        'strict' => false,
+        'engine' => null,
+    ],
 
+    'pgsql' => [
+        'driver' => 'pgsql',
+        'host' => env('DB_HOST', 'localhost'),
+        'port' => env('DB_PORT', '5432'),
+        'database' => env('DB_DATABASE', 'forge'),
+        'username' => env('DB_USERNAME', 'forge'),
+        'password' => env('DB_PASSWORD', ''),
+        'charset' => 'utf8',
+        'prefix' => '',
+        'schema' => 'public',
     ],
 
     'migrations' => 'migrations'
